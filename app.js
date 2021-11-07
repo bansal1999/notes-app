9a8654f4935f37271ff2224870c6615bb5f8bf10
Shikhar Bansal <shikharbansal007@gmail.com>
2021-11-07 11:12:44 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const yargs = require("yargs");
+const chalk = require("chalk");
+const notes = require("./notes");
+const { demandOption } = require("yargs");
+
+//Create ADD Command
+yargs.command({
+  command: "add",
+  description: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+
+    body: {
+      description: "Note Body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body);
+  },
+});
+
+//Create REMOVE Command
+yargs.command({
+  command: "remove",
+  description: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler(argv) {
+    notes.removeNote(argv.title);
+  },
+});
+
+//Creating list command
+yargs.command({
+  command: "list",
+  description: "List all notes",
+  handler() {
+    notes.listNotes();
+  },
+});
+
+//Create Read Command
+yargs.command({
+  command: "read",
+  description: "Read a Note",
+  handler() {
+    console.log("Reading a note");
+  },
+});
+
+yargs.parse();
